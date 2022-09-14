31d96f4654bafee94b5ca606db2c434053040400
Hernán G <hernang.vp@gmail.com>
2022-09-14 02:43:00 -0300

    Botón de 'Guarda y empezar' graba nueva palabra en 'arrayPalabas'

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
 /* APARECE SOLAMENTE LA PANTALLA DE PRESENTACIÓN DEL JUEGO */
-const presentacion = document.getElementById("presentation").style.display = "flex";
+const presentacion = document.getElementById("openGame").style.display = "flex";
 const nuevaPalabra = document.getElementById("new-word").style.display = "none";
 const nuevoJuego = document.getElementById("new-game").style.display = "none";
+
+const input = document.querySelector(".new-word__input");
+const advertencia = document.querySelector(".warning");
+
+let arrayPalabras = ["alura", "oracle", "html", "css", "challenge", "one", "javascript"]
 
 
 
 function entrar_nuevoJuego() {
-    document.getElementById("presentation").style.display = "none";
+    document.getElementById("openGame").style.display = "none";
     document.getElementById("new-game").style.display = "flex";
 }
 
 function agregar_nuevaPalabra() {
-    document.getElementById("presentation").style.display = "none";
+    document.getElementById("openGame").style.display = "none";
     document.getElementById("new-word").style.display = "flex";
 }
 
 /* PANTALLA AGREGAR PALABRA NUEVA */
+function clearInputText() {
+    input.value = ""
+}
+
 function guardarEmpezar() {
-    document.getElementById("new-word").style.display = "none";
-    document.getElementById("new-game").style.display = "flex";
+    if (input.value == "") {
+        // document.getElementById("new-word").style.display = "none";
+        document.getElementById("new-game").style.display = "flex";
+        advertencia.innerText = "⚠ Ingrese una palabra o presione Cancelar"
+    } else {
+        arrayPalabras.push(input.value);
+        clearInputText();
+        advertencia.innerText = "";
+        console.log(arrayPalabras); /* borrar console.log */
+    }
 }
 
 function cancelar_nuevaPalabra() {
     document.getElementById("new-word").style.display = "none";
-    document.getElementById("presentation").style.display = "flex";
+    document.getElementById("openGame").style.display = "flex";
+    clearInputText()
 }
 
 /* PANTALLA JUEGO NUEVO */
 
- 
-
-
 function salir_nuevoJuego() {
     document.getElementById("new-game").style.display = "none";
-    document.getElementById("presentation").style.display = "flex";
+    document.getElementById("openGame").style.display = "flex";
+}
+
+
+function prueba() {
+    let prueba = `
+    <h1>hola</h1>
+  `
+    console.log(prueba)
 }
