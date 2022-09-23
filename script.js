89f9920ec5974f31461e1a5979f726ab03db7c43
Hernán G <hernang.vp@gmail.com>
2022-09-23 18:47:24 -0300

    FPantallas y botones funcionando. Falta reemplazar los guiones por las letras y poner el dibujo del ahorcado

--- script.js
+++ script.js
@@ -3,45 +3,50 @@ const presentacion = document.getElementById("openGame").style.display = "flex";
 const nuevaPalabra = document.getElementById("new-word").style.display = "none";
 const nuevoJuego = document.getElementById("new-game").style.display = "none";
 
+/* INPUT, ARRAY Y ADVERTENCIA DE LA PANTALLA AGREGAR PALABRA */
 const input = document.querySelector(".new-word__input");
 const advertencia = document.querySelector(".warning");
-
 const arrayPalabras = ["alura", "oracle", "html", "css", "challenge", "one", "javascript"]
 
-/////////////////////////////
+/* PALABRA AL AZAR CON GUIONES */
 function palabraNueva() {
-const palabraAleatoria = arrayPalabras[Math.floor(Math.random() * arrayPalabras.length)].toUpperCase();
-console.log(palabraAleatoria)
-let palabraGuiones = palabraAleatoria.replace(/./g,"_ ")
-console.log(palabraGuiones)
-let hiddenWord = document.querySelector(".hidden-word")
-hiddenWord.innerText = palabraGuiones;
+    const palabraAleatoria = arrayPalabras[Math.floor(Math.random() * arrayPalabras.length)].toUpperCase();
+    console.log(palabraAleatoria)
+    let palabraGuiones = palabraAleatoria.replace(/./g, "_")
+    console.log(palabraGuiones)
+    let hiddenWord = document.querySelector(".hidden-word")
+    hiddenWord.innerText = palabraGuiones;
 }
-//////////////////////////////
+
+/* EVENTO PARA CAPTURAR LAS LETRAS DEL TECLADO */
+const letterPress = event => {
+    let newLetter = event.key.toLocaleUpperCase();
+    if (newLetter.match(/^[a-zñ]$/i)) { //solo toma caracteres alfabéticos, sin números ni símbolos
+        console.log("letra " + newLetter);
+    }
+}
+
+/* EVENTO PARA DEJAR DE CAPTURAR EL TECLADO */
+function noLetterCapture() {
+    document.removeEventListener("keydown", letterPress);
+
+}
 
 
-
-
+/* PANTALLA INICIAL */
 function entrar_nuevoJuego() {
     document.getElementById("openGame").style.display = "none";
     document.getElementById("new-game").style.display = "flex";
     palabraNueva()
-    document.addEventListener("keydown", letterEvent);
-}
-
-//evento para capturar las letras del teclado
-const letterEvent = event => {
-    let newLetter = event.key.toLocaleUpperCase();
-    if(newLetter.match(/^[a-zñ]$/i)) {
-        console.log("letra" + newLetter);
-    }
+    document.addEventListener("keydown", letterPress);
 }
 
 function agregar_nuevaPalabra() {
     document.getElementById("openGame").style.display = "none";
     document.getElementById("new-word").style.display = "flex";
-    document.querySelector(".new-word__input").focus();
+    input.focus();
 }
+
 
 /* PANTALLA AGREGAR PALABRA NUEVA */
 function clearInputText() {
@@ -51,7 +56,7 @@ function clearInputText() {
 function guardarEmpezar() {
     if (input.value == "") {
         advertencia.innerText = "⚠ Ingrese una palabra o presione Cancelar"
-        document.querySelector(".new-word__input").focus()
+        input.focus()
     } else {
         document.getElementById("new-word").style.display = "none";
         document.getElementById("new-game").style.display = "flex";
@@ -59,6 +64,7 @@ function guardarEmpezar() {
         clearInputText();
         advertencia.innerText = "";
         palabraNueva()
+        document.addEventListener("keydown", letterPress);
         console.log(arrayPalabras); /* borrar console.log */
     }
 }
@@ -69,9 +75,10 @@ function cancelar_nuevaPalabra() {
     clearInputText()
 }
 
-/* PANTALLA JUEGO NUEVO */
 
+/* PANTALLA AHORACADO */
 function salir_nuevoJuego() {
     document.getElementById("new-game").style.display = "none";
     document.getElementById("openGame").style.display = "flex";
+    noLetterCapture() //deja de capturar el teclado
 }
