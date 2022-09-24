e16e52f6d75af38d045c0f51ab8973a0f53e89c5
Hernán G <hernang.vp@gmail.com>
2022-09-24 05:09:30 -0300

    Queda reemplazar los guiones por las letras y poner el dibujo del ahorcado

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function palabraNueva() {
 }
 
 /* EVENTO PARA CAPTURAR LAS LETRAS DEL TECLADO */
-const letterPress = event => {
+const capturarLetra = event => {
     let newLetter = event.key.toLocaleUpperCase();
     if (newLetter.match(/^[a-zñ]$/i)) { //solo toma caracteres alfabéticos, sin números ni símbolos
         console.log("letra " + newLetter);
@@ -27,18 +27,17 @@ const letterPress = event => {
 }
 
 /* EVENTO PARA DEJAR DE CAPTURAR EL TECLADO */
-function noLetterCapture() {
-    document.removeEventListener("keydown", letterPress);
-
+function noCapturarLetra() {
+    document.removeEventListener("keydown", capturarLetra);
 }
 
 
-/* PANTALLA INICIAL */
+///* PANTALLA INICIAL *///
 function entrar_nuevoJuego() {
     document.getElementById("openGame").style.display = "none";
     document.getElementById("new-game").style.display = "flex";
     palabraNueva()
-    document.addEventListener("keydown", letterPress);
+    document.addEventListener("keydown", capturarLetra);
 }
 
 function agregar_nuevaPalabra() {
@@ -48,11 +47,30 @@ function agregar_nuevaPalabra() {
 }
 
 
-/* PANTALLA AGREGAR PALABRA NUEVA */
+///* PANTALLA AGREGAR PALABRA NUEVA *///
 function clearInputText() {
     input.value = ""
 }
 
+/* EVENTO 'ENTER' PARA GRABAR PALABRA NUEVA EN EL INPUT Y COMENZAR UNA NUEVA PARTIDA */
+input.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        guardarEmpezar();
+    }
+});
+
+/* EVENTO 'ESCAPE' PARA SALIR A LA PANTALLA PRINCIPAL */
+document.addEventListener('keydown', function (event) {
+    if (event.code == 'Escape') {
+        salir_nuevoJuego();        
+        document.getElementById("new-word").style.display = "none";
+        advertencia.innerText = "";
+    }
+});
+
+
+/* BOTÓN GUARDAR Y EMPEZAR */
 function guardarEmpezar() {
     if (input.value == "") {
         advertencia.innerText = "⚠ Ingrese una palabra o presione Cancelar"
@@ -64,21 +82,50 @@ function guardarEmpezar() {
         clearInputText();
         advertencia.innerText = "";
         palabraNueva()
-        document.addEventListener("keydown", letterPress);
+        document.addEventListener("keydown", capturarLetra);
         console.log(arrayPalabras); /* borrar console.log */
     }
 }
 
+/* BOTÓN CANCELAR NUEVA PALABRA Y VOLVER A LA PANTALLA PRINCIPAL */
 function cancelar_nuevaPalabra() {
     document.getElementById("new-word").style.display = "none";
     document.getElementById("openGame").style.display = "flex";
     clearInputText()
+    advertencia.innerText = "";
 }
 
 
-/* PANTALLA AHORACADO */
+///* PANTALLA AHORACADO *///
 function salir_nuevoJuego() {
     document.getElementById("new-game").style.display = "none";
     document.getElementById("openGame").style.display = "flex";
-    noLetterCapture() //deja de capturar el teclado
+    noCapturarLetra() //deja de capturar el teclado
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Requisitos:
+- Al completar el dibujo de la horca, debe ser mostrado un mensaje "Fin del juego" en la pantalla;
+- Si se completa la palabra correcta antes de que se acaben los intentos, debe ser mostrado un mensaje de "Ganaste, Felicidades!" en la pantalla.
+- La página debe tener los guiones indicando cada letra da palabra, separados por un espacio;
+- Las letras equivocadas deben aparecer en la pantalla, pero no pueden aparecer de forma repetida;
+- Las letras correctas deben aparecer en la pantalla encima de los guiones, en la posición correcta em relación a la palabra.
+
+
++ Debe funcionar solo con letras mayúsculas;
++ Para comenzar el juego la página debe tener un botón de "Iniciar Juego";
++ No deben ser utilizadas letras con acentos ni caracteres especiales;
++ No debe ser posible escribir números dentro del juego.
++ La página debe tener un campo para inserción de texto con la finalidad de adicionar nuevas palabras al juego, e un botón "Agregar palabra". */
