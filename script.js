266d9adea4a054560b764ba1287bb21155546666
Hernán G <hernang.vp@gmail.com>
2022-09-25 21:56:06 -0300

    Nuevo proyecto casi terminado. Falta el contador de errores

---
+++ script.js
@@ -0,0 +1,166 @@
+String.prototype.replaceAt = function (i, letra) {
+    return this.substring(0, i) + letra + this.substring(i + letra.length);
+}
+
+/* APARECE SOLAMENTE LA PANTALLA DE PRESENTACIÓN DEL JUEGO */
+const presentacion = document.getElementById("openGame").style.display = "flex";
+const nuevaPalabra = document.getElementById("new-word").style.display = "none";
+const nuevoJuego = document.getElementById("new-game").style.display = "none";
+
+const arrayPalabras = ["gato", "perro", "elefante", "conejo", "lemur", "gorila", "ardilla", "pato", "gallina"];
+
+let palabra = null
+let palabraConGuiones = null
+
+const enfocar = document.querySelector(".ingresarLetra");
+let dibujoAhorcado = document.querySelector("#ahorcado");
+const juegoGanado = document.querySelector("#won-game");
+/* //////////////////////////////////////////////////////////// */
+/* function palabraNueva() {
+    const palabraAleatoria = arrayPalabras[Math.floor(Math.random() * arrayPalabras.length)].toUpperCase();
+    console.log(palabraAleatoria)
+    let palabraGuiones = palabraAleatoria.replace(/./g, "_")
+    console.log(palabraGuiones)
+    let hiddenWord = document.querySelector(".hidden-word")
+    hiddenWord.innerText = palabraGuiones;
+} */
+/* //////////////////////////////////////////////////////////// */
+
+function palabraNueva() {
+    palabra = arrayPalabras[(Math.floor(Math.random() * arrayPalabras.length))];
+    console.log(palabra)
+    palabraConGuiones = palabra.replace(/./g, "_ ");
+    console.log(palabraConGuiones)
+    document.querySelector(".output").innerHTML = palabraConGuiones;
+    // dibujoAhorcado.style.backgroundPosition = 0;
+    enfocar.focus();
+    juegoGanado.style.display = "none"
+}
+/* //////////////////////////////////////////////////////////// */
+
+
+// let palabra = arrayPalabras[(Math.floor(Math.random() * arrayPalabras.length))];
+
+// let palabraConGuiones = palabra.replace(/./g, "_ ");
+
+let contadorFallos = 0;
+
+// console.log(palabra)
+
+// document.querySelector(".output").innerHTML = palabraConGuiones;
+
+
+const escribirLetra = document.querySelector(".ingresarLetra");
+
+
+escribirLetra.addEventListener("keyup", function (event) {
+    let newLetter = event.key.toLocaleUpperCase();
+    if (newLetter.match(/^[a-zñ]$/i)) {
+        console.log(newLetter)
+    }
+
+    let letra = document.querySelector(".ingresarLetra").value;
+    let haFallado = true;
+    for (let i in palabra) {
+        if (letra == palabra[i]) {
+            palabraConGuiones = palabraConGuiones.replaceAt(i * 2, letra);
+            haFallado = false;
+        }
+    }
+
+    if (haFallado) {
+        contadorFallos++;
+        dibujoAhorcado.style.backgroundPosition = -(199 * contadorFallos) + 'px 0';
+        if (contadorFallos == 4) {
+            alert("perdiste el juego")
+        }
+    } else {        
+        if (palabraConGuiones.indexOf("_") < 0) {
+            juegoGanado.style.display = "flex"
+            document.querySelector(".won-game__word").innerHTML = palabra.toUpperCase();
+        }
+    }
+
+    document.querySelector(".output").innerHTML = palabraConGuiones;
+
+    enfocar.value = ""
+    enfocar.focus();
+});
+/* //////////////////////////////////////////////////////////// */
+
+
+
+/* INPUT, ARRAY Y ADVERTENCIA DE LA PANTALLA AGREGAR PALABRA */
+const input = document.querySelector(".new-word__input");
+const advertencia = document.querySelector(".warning");
+
+///* PANTALLA INICIAL *///
+function entrar_nuevoJuego() {    
+    document.getElementById("openGame").style.display = "none";
+    document.getElementById("new-game").style.display = "flex";
+    palabraNueva()
+    enfocar.focus();
+    // palabraSecretaAleatoria()
+    // document.addEventListener("keydown", capturarLetra);
+}
+
+function agregar_nuevaPalabra() {
+    document.getElementById("openGame").style.display = "none";
+    document.getElementById("new-word").style.display = "flex";
+    input.focus();
+}
+
+///* PANTALLA AGREGAR PALABRA NUEVA *///
+function clearInputText() {
+    input.value = ""
+}
+
+/* EVENTO 'ENTER' PARA GRABAR PALABRA NUEVA EN EL INPUT Y COMENZAR UNA NUEVA PARTIDA */
+input.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        guardarEmpezar();
+    }
+});
+
+/* EVENTO 'ESCAPE' PARA SALIR A LA PANTALLA PRINCIPAL */
+document.addEventListener('keydown', function (event) {
+    if (event.code == 'Escape') {
+        salir_nuevoJuego();        
+        document.getElementById("new-word").style.display = "none";
+        advertencia.innerText = "";
+        input.value = ""
+    }
+});
+
+/* BOTÓN GUARDAR Y EMPEZAR */
+function guardarEmpezar() {
+    if (input.value == "") {
+        advertencia.innerText = "⚠ Ingrese una palabra o presione Cancelar"
+        input.focus()
+    } else {
+        document.getElementById("new-word").style.display = "none";
+        document.getElementById("new-game").style.display = "flex";
+        arrayPalabras.push(input.value);
+        clearInputText();
+        advertencia.innerText = "";
+        // palabraNueva()
+        // document.addEventListener("keydown", capturarLetra);
+        console.log(arrayPalabras); /* borrar console.log */
+    }
+}
+
+/* BOTÓN CANCELAR NUEVA PALABRA Y VOLVER A LA PANTALLA PRINCIPAL */
+function cancelar_nuevaPalabra() {
+    document.getElementById("new-word").style.display = "none";
+    document.getElementById("openGame").style.display = "flex";
+    clearInputText()
+    advertencia.innerText = "";
+}
+
+///* PANTALLA AHORACADO *///
+function salir_nuevoJuego() {
+    document.getElementById("new-game").style.display = "none";
+    document.getElementById("openGame").style.display = "flex";
+    // noCapturarLetra() //deja de capturar el teclado
+}
